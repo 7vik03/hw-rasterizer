a14d1e2ffc91fd62db8110ac55af75310de79718
srikac <64172990+srikac@users.noreply.github.com>
2026-04-15 13:18:41 -0400

    Create vga_framebuffer.sv

---
+++ hardware/vga_framebuffer.sv
@@ -0,0 +1,119 @@
+/*
+ * Avalon memory-mapped peripheral that generates VGA
+
+ */
+
+module vga_framebuffer(input logic        clk,
+	        input logic 	   reset,
+
+		output logic [7:0] VGA_R, VGA_G, VGA_B,
+		output logic 	   VGA_CLK, VGA_HS, VGA_VS,
+		                   VGA_BLANK_n,
+		output logic 	   VGA_SYNC_n);
+
+   logic [10:0]	   hcount;
+   logic [9:0]     vcount;
+   logic [8:0] fb_x;
+   logic [7:0] fb_y;
+   logic [16:0] fb_addr;
+   logic [7:0] pixelcolor;
+   logic [7:0] framebuffer [0:320*240-1];
+
+   vga_counters counters(.clk50(clk), .reset(reset), .*);
+   assign fb_x = hcount[10:2];
+   assign fb_y = vcount[9:1];
+   assign fb_addr = (fb_y << 8) + (fb_y << 6) + fb_x;
+   assign pixelcolor = framebuffer[fb_addr];
+integer i;
+initial begin
+    for (i = 0; i < 320*240; i = i + 1)
+        framebuffer[i] = 8'h00;
+end
+      always_comb begin
+    {VGA_R, VGA_G, VGA_B} = 24'h0;
+    if (VGA_BLANK_n) begin
+      VGA_R = {pixelcolor[7:5], pixelcolor[7:5], pixelcolor[7:6]};
+         VGA_G = {pixelcolor[4:2], pixelcolor[4:2], pixelcolor[4:3]};
+         VGA_B = {pixelcolor[1:0], pixelcolor[1:0], pixelcolor[1:0], pixelcolor[1:0]};
+    end
+end
+endmodule
+
+module vga_counters(
+ input logic 	     clk50, reset,
+ output logic [10:0] hcount,  // hcount[10:1] is pixel column
+ output logic [9:0]  vcount,  // vcount[9:0] is pixel row
+ output logic 	     VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_n, VGA_SYNC_n);
+
+/*
+ * 640 X 480 VGA timing for a 50 MHz clock: one pixel every other cycle
+ * 
+ * HCOUNT 1599 0             1279       1599 0
+ *             _______________              ________
+ * ___________|    Video      |____________|  Video
+ * 
+ * 
+ * |SYNC| BP |<-- HACTIVE -->|FP|SYNC| BP |<-- HACTIVE
+ *       _______________________      _____________
+ * |____|       VGA_HS          |____|
+ */
+   // Parameters for hcount
+   parameter HACTIVE      = 11'd 1280,
+             HFRONT_PORCH = 11'd 32,
+             HSYNC        = 11'd 192,
+             HBACK_PORCH  = 11'd 96,   
+             HTOTAL       = HACTIVE + HFRONT_PORCH + HSYNC +
+                            HBACK_PORCH; // 1600
+   
+   // Parameters for vcount
+   parameter VACTIVE      = 10'd 480,
+             VFRONT_PORCH = 10'd 10,
+             VSYNC        = 10'd 2,
+             VBACK_PORCH  = 10'd 33,
+             VTOTAL       = VACTIVE + VFRONT_PORCH + VSYNC +
+                            VBACK_PORCH; // 525
+
+   logic endOfLine;
+   
+   always_ff @(posedge clk50 or posedge reset)
+     if (reset)          hcount <= 0;
+     else if (endOfLine) hcount <= 0;
+     else  	         hcount <= hcount + 11'd 1;
+
+   assign endOfLine = hcount == HTOTAL - 1;
+       
+   logic endOfField;
+   
+   always_ff @(posedge clk50 or posedge reset)
+     if (reset)          vcount <= 0;
+     else if (endOfLine)
+       if (endOfField)   vcount <= 0;
+       else              vcount <= vcount + 10'd 1;
+
+   assign endOfField = vcount == VTOTAL - 1;
+
+   // Horizontal sync: from 0x520 to 0x5DF (0x57F)
+   // 101 0010 0000 to 101 1101 1111
+   assign VGA_HS = !( (hcount[10:8] == 3'b101) &
+		      !(hcount[7:5] == 3'b111));
+   assign VGA_VS = !( vcount[9:1] == (VACTIVE + VFRONT_PORCH) / 2);
+
+   assign VGA_SYNC_n = 1'b0; // For putting sync on the green signal; unused
+   
+   // Horizontal active: 0 to 1279     Vertical active: 0 to 479
+   // 101 0000 0000  1280	       01 1110 0000  480
+   // 110 0011 1111  1599	       10 0000 1100  524
+   assign VGA_BLANK_n = !( hcount[10] & (hcount[9] | hcount[8]) ) &
+			!( vcount[9] | (vcount[8:5] == 4'b1111) );
+
+   /* VGA_CLK is 25 MHz
+    *             __    __    __
+    * clk50    __|  |__|  |__|
+    *        
+    *             _____       __
+    * hcount[0]__|     |_____|
+    */
+   assign VGA_CLK = hcount[0]; // 25 MHz clock: rising edge sensitive
+   
+endmodule
+
