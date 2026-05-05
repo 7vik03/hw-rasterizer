94c2d86241ab1d5778942c5f2c87dca13a42bb13
Gianna <giannabelmont@gmail.com>
2026-05-05 19:14:00 -0400

    avalon renames

--- sim/rasterizer_top_tb.sv
+++ sim/rasterizer_top_tb.sv
@@ -9,20 +9,22 @@ module rasterizer_top_tb;
 
     logic        clk;
     logic        rst;
-    logic [6:0]  avalon_address;
-    logic        avalon_write;
-    logic [31:0] avalon_writedata;
-    logic [31:0] avalon_readdata;
+    logic [6:0]  avs_address;
+    logic        avs_write;
+    logic [31:0] avs_writedata;
+    logic [31:0] avs_readdata;
+    logic        avs_waitrequest;
     logic [7:0]  VGA_R, VGA_G, VGA_B;
     logic        VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_n, VGA_SYNC_n;
 
     rasterizer_top dut (
         .clk              (clk),
         .rst              (rst),
-        .avalon_address   (avalon_address),
-        .avalon_write     (avalon_write),
-        .avalon_writedata (avalon_writedata),
-        .avalon_readdata  (avalon_readdata),
+        .avs_address      (avs_address),
+        .avs_write        (avs_write),
+        .avs_writedata    (avs_writedata),
+        .avs_readdata     (avs_readdata),
+        .avs_waitrequest  (avs_waitrequest),
         .VGA_R            (VGA_R),
         .VGA_G            (VGA_G),
         .VGA_B            (VGA_B),
@@ -39,10 +41,10 @@ module rasterizer_top_tb;
     end
 
     initial begin
-        rst              = 1;
-        avalon_address   = '0;
-        avalon_write     = 1'b0;
-        avalon_writedata = '0;
+        rst           = 1;
+        avs_address   = '0;
+        avs_write     = 1'b0;
+        avs_writedata = '0;
         repeat (5) @(posedge clk);
         rst = 0;
         // let the design settle, VGA counters run through some lines
